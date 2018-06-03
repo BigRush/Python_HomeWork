34e0e5974871f9b084b6da08fbc29ada8d1c533a
BigRush <tom.harpaz98@gmail.com>
2018-06-03 03:07:11 +0300

    added git ignore

--- Ex3/ex2.py
+++ Ex3/ex2.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python3.6
 
-def List_Sum (list):
-    for i in list:
-        j += int(i)
-    return j
+list = []
+def User_Input (list):
+    try:
+        j = 0
+        user_num = int(input("Please anter a number: "))
+        list.append(user_num)
+        print("List now: {}".format(list))
+        for i in list:
+            print ("i now: {}".format(i))
+            j += int(i)
+            if j > 30:
+                print("Summary: {}".format(j))
+                break
+        while user_num < 10:
+            try:
+                j = 0
+                user_num = int(input("Please anter a number: "))
+                list.append(user_num)
+                print("List now: {}".format(list))
+                for i in list:
+                    print ("i now: {}".format(i))
+                    j += int(i)
+                    if j > 30:
+                        print("Summary: {}".format(j))
+                        break
+            except:
+                print("Invalid number plese try again")
+                User_Input (list)
 
-def User_Input (j):
-    list = []
-    j = 0
-    while j <= 30:
-        #try:
-        user_num=int(input("Please enter a number: "))
-        if user_num < 10:
-            list += user_num
-            j = List_Sum(list)
-        else:
-            break
+        while user_num < 10 or j < 30:
+            User_Input (list)
 
-    #    except:
-    #        print("Invalid number plese try again")
+        print(j)
+        exit()
+    except:
+        print("Invalid number plese try again")
+        User_Input (list)
+
+#user_num = User_Input ()
 
 
-User_Input (j)
+
+
+
+
+
+User_Input(list)
